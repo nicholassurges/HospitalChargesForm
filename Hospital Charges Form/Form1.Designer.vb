f31f4232bb340636afd108d538cdd64ad0421231
nicholassurges <81642890+nicholassurges@users.noreply.github.com>
2022-03-31 21:28:24 -0400

    Add files via upload

---
+++ Hospital Charges Form/Form1.Designer.vb
@@ -0,0 +1,243 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnCalc = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.txtRehab = New System.Windows.Forms.TextBox()
+        Me.txtLabFees = New System.Windows.Forms.TextBox()
+        Me.txtSurgicalCharges = New System.Windows.Forms.TextBox()
+        Me.txtMedication = New System.Windows.Forms.TextBox()
+        Me.txtDays = New System.Windows.Forms.TextBox()
+        Me.lblPhysicalRehab = New System.Windows.Forms.Label()
+        Me.lblLabFees = New System.Windows.Forms.Label()
+        Me.lblSurgicalCharges = New System.Windows.Forms.Label()
+        Me.lblMedication = New System.Windows.Forms.Label()
+        Me.lblLenghtOfStay = New System.Windows.Forms.Label()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.lblTotal = New System.Windows.Forms.Label()
+        Me.lblTotalCost = New System.Windows.Forms.Label()
+        Me.Panel1.SuspendLayout()
+        Me.Panel2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'btnCalc
+        '
+        Me.btnCalc.Location = New System.Drawing.Point(12, 372)
+        Me.btnCalc.Name = "btnCalc"
+        Me.btnCalc.Size = New System.Drawing.Size(109, 66)
+        Me.btnCalc.TabIndex = 0
+        Me.btnCalc.Text = "Calculate Charges"
+        Me.btnCalc.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(138, 372)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(109, 66)
+        Me.btnClear.TabIndex = 1
+        Me.btnClear.Text = "Clear Form"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(259, 372)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(109, 66)
+        Me.btnExit.TabIndex = 2
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.SystemColors.ActiveBorder
+        Me.Panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Panel1.Controls.Add(Me.txtRehab)
+        Me.Panel1.Controls.Add(Me.txtLabFees)
+        Me.Panel1.Controls.Add(Me.txtSurgicalCharges)
+        Me.Panel1.Controls.Add(Me.txtMedication)
+        Me.Panel1.Controls.Add(Me.txtDays)
+        Me.Panel1.Controls.Add(Me.lblPhysicalRehab)
+        Me.Panel1.Controls.Add(Me.lblLabFees)
+        Me.Panel1.Controls.Add(Me.lblSurgicalCharges)
+        Me.Panel1.Controls.Add(Me.lblMedication)
+        Me.Panel1.Controls.Add(Me.lblLenghtOfStay)
+        Me.Panel1.Location = New System.Drawing.Point(26, 12)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(319, 230)
+        Me.Panel1.TabIndex = 9
+        '
+        'txtRehab
+        '
+        Me.txtRehab.Location = New System.Drawing.Point(197, 186)
+        Me.txtRehab.Name = "txtRehab"
+        Me.txtRehab.Size = New System.Drawing.Size(100, 22)
+        Me.txtRehab.TabIndex = 9
+        '
+        'txtLabFees
+        '
+        Me.txtLabFees.Location = New System.Drawing.Point(197, 140)
+        Me.txtLabFees.Name = "txtLabFees"
+        Me.txtLabFees.Size = New System.Drawing.Size(100, 22)
+        Me.txtLabFees.TabIndex = 8
+        '
+        'txtSurgicalCharges
+        '
+        Me.txtSurgicalCharges.Location = New System.Drawing.Point(197, 102)
+        Me.txtSurgicalCharges.Name = "txtSurgicalCharges"
+        Me.txtSurgicalCharges.Size = New System.Drawing.Size(100, 22)
+        Me.txtSurgicalCharges.TabIndex = 7
+        '
+        'txtMedication
+        '
+        Me.txtMedication.Location = New System.Drawing.Point(194, 58)
+        Me.txtMedication.Name = "txtMedication"
+        Me.txtMedication.Size = New System.Drawing.Size(100, 22)
+        Me.txtMedication.TabIndex = 6
+        '
+        'txtDays
+        '
+        Me.txtDays.Location = New System.Drawing.Point(194, 11)
+        Me.txtDays.Name = "txtDays"
+        Me.txtDays.Size = New System.Drawing.Size(100, 22)
+        Me.txtDays.TabIndex = 5
+        '
+        'lblPhysicalRehab
+        '
+        Me.lblPhysicalRehab.AutoSize = True
+        Me.lblPhysicalRehab.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblPhysicalRehab.Location = New System.Drawing.Point(63, 189)
+        Me.lblPhysicalRehab.Name = "lblPhysicalRehab"
+        Me.lblPhysicalRehab.Size = New System.Drawing.Size(125, 17)
+        Me.lblPhysicalRehab.TabIndex = 4
+        Me.lblPhysicalRehab.Text = "Physical Rehab:"
+        '
+        'lblLabFees
+        '
+        Me.lblLabFees.AutoSize = True
+        Me.lblLabFees.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblLabFees.Location = New System.Drawing.Point(108, 145)
+        Me.lblLabFees.Name = "lblLabFees"
+        Me.lblLabFees.Size = New System.Drawing.Size(80, 17)
+        Me.lblLabFees.TabIndex = 3
+        Me.lblLabFees.Text = "Lab Fees:"
+        '
+        'lblSurgicalCharges
+        '
+        Me.lblSurgicalCharges.AutoSize = True
+        Me.lblSurgicalCharges.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblSurgicalCharges.Location = New System.Drawing.Point(51, 105)
+        Me.lblSurgicalCharges.Name = "lblSurgicalCharges"
+        Me.lblSurgicalCharges.Size = New System.Drawing.Size(137, 17)
+        Me.lblSurgicalCharges.TabIndex = 2
+        Me.lblSurgicalCharges.Text = "Surgical Charges:"
+        '
+        'lblMedication
+        '
+        Me.lblMedication.AutoSize = True
+        Me.lblMedication.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblMedication.Location = New System.Drawing.Point(97, 61)
+        Me.lblMedication.Name = "lblMedication"
+        Me.lblMedication.Size = New System.Drawing.Size(91, 17)
+        Me.lblMedication.TabIndex = 1
+        Me.lblMedication.Text = "Medication:"
+        '
+        'lblLenghtOfStay
+        '
+        Me.lblLenghtOfStay.AutoSize = True
+        Me.lblLenghtOfStay.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblLenghtOfStay.Location = New System.Drawing.Point(16, 14)
+        Me.lblLenghtOfStay.Name = "lblLenghtOfStay"
+        Me.lblLenghtOfStay.Size = New System.Drawing.Size(172, 17)
+        Me.lblLenghtOfStay.TabIndex = 0
+        Me.lblLenghtOfStay.Text = "Length of Stay (Days):"
+        '
+        'Panel2
+        '
+        Me.Panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Panel2.Controls.Add(Me.lblTotal)
+        Me.Panel2.Controls.Add(Me.lblTotalCost)
+        Me.Panel2.Location = New System.Drawing.Point(26, 266)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(319, 73)
+        Me.Panel2.TabIndex = 0
+        '
+        'lblTotal
+        '
+        Me.lblTotal.AutoSize = True
+        Me.lblTotal.Font = New System.Drawing.Font("Palatino Linotype", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTotal.Location = New System.Drawing.Point(194, 22)
+        Me.lblTotal.Name = "lblTotal"
+        Me.lblTotal.Size = New System.Drawing.Size(0, 27)
+        Me.lblTotal.TabIndex = 1
+        '
+        'lblTotalCost
+        '
+        Me.lblTotalCost.AutoSize = True
+        Me.lblTotalCost.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTotalCost.Location = New System.Drawing.Point(16, 22)
+        Me.lblTotalCost.Name = "lblTotalCost"
+        Me.lblTotalCost.Size = New System.Drawing.Size(102, 20)
+        Me.lblTotalCost.TabIndex = 0
+        Me.lblTotalCost.Text = "Total Cost:"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(380, 450)
+        Me.Controls.Add(Me.Panel2)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnCalc)
+        Me.Name = "Form1"
+        Me.Text = "Hospital Charges"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.Panel2.ResumeLayout(False)
+        Me.Panel2.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents btnCalc As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents lblPhysicalRehab As Label
+    Friend WithEvents lblLabFees As Label
+    Friend WithEvents lblSurgicalCharges As Label
+    Friend WithEvents lblMedication As Label
+    Friend WithEvents lblLenghtOfStay As Label
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents txtRehab As TextBox
+    Friend WithEvents txtLabFees As TextBox
+    Friend WithEvents txtSurgicalCharges As TextBox
+    Friend WithEvents txtMedication As TextBox
+    Friend WithEvents txtDays As TextBox
+    Friend WithEvents lblTotal As Label
+    Friend WithEvents lblTotalCost As Label
+End Class
---
+++ Hospital Charges Form/Form1.vb
@@ -0,0 +1,127 @@
+﻿Public Class Form1
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        'Close Form
+        Me.Close()
+    End Sub
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        'Clear all input fields
+        txtDays.Clear()
+        txtMedication.Clear()
+        txtLabFees.Clear()
+        txtRehab.Clear()
+        txtSurgicalCharges.Clear()
+        lblTotal.Text = String.Empty
+        txtDays.Focus()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnCalc.Click
+        If ValidateInputFields() Then
+
+            Dim intNumDays As Integer = CInt(txtDays.Text)
+            Dim decMeds As Decimal = CDec(txtMedication.Text)
+            Dim decLabFees As Decimal = CDec(txtLabFees.Text)
+            Dim decSurgery As Decimal = CDec(txtSurgicalCharges.Text)
+            Dim decRehab As Decimal = CDec(txtRehab.Text)
+            Dim decTotal As Decimal
+
+            decTotal = (intNumDays * 350) + decMeds + decLabFees + decSurgery + decRehab
+
+            Dim decDailyCost As Decimal
+
+            Dim decLabFeeCost As Decimal = 0
+            Dim RehabCost As Decimal = 0
+            Dim decTotalCost As Decimal = 0
+            Dim decMiscCosts As Decimal = 0
+
+            decDailyCost = CalcStayCharges(intNumDays)
+
+            decMiscCosts = CalcMiscCharges(decMeds, decLabFees, decSurgery, decRehab)
+
+            decTotalCost = CalcTotalCharges(decDailyCost, decMiscCosts)
+
+            lblTotal.Text = decTotalCost.ToString("c")
+
+        End If
+    End Sub
+    Function CalcStayCharges(ByVal intNumDays As Integer) As Integer
+        Return intNumDays * 350
+
+    End Function
+
+    Function CalcMiscCharges(ByVal decMeds As Decimal, ByVal decLabFees As Decimal, ByVal decSurgery As Decimal,
+                             ByVal decRehab As Decimal) As Decimal
+        Return decMeds + decLabFees + decSurgery + decRehab
+    End Function
+
+    Function CalcTotalCharges(ByVal decDailyCost As Decimal, ByVal miscCosts As Decimal) As Decimal
+        Return decDailyCost + miscCosts
+    End Function
+
+    Function ValidateInputFields() As Boolean
+        Dim intTryParse As Integer = CInt(txtDays.Text)
+        Dim decTryParse As Decimal
+
+        If txtDays.Text = "" Then
+            MessageBox.Show("You must enter the number Of days As a positive number")
+            Return False
+        ElseIf Not Integer.TryParse(txtDays.Text, intTryParse) Then
+            MessageBox.Show("Input Error You must enter positive number")
+        ElseIf CInt(txtDays.Text) < 0 Then
+            MessageBox.Show("Input Error the number cannot be negative.")
+            Return False
+
+        End If
+
+
+        If txtMedication.Text = "" Then
+            MessageBox.Show("You must enter the cost of medication as a decimal number")
+            Return False
+        ElseIf Not Decimal.TryParse(txtMedication.Text, decTryParse) Then
+            MessageBox.Show("Invalid Input Medication Cost must be entered as a decimal")
+            Return False
+        ElseIf CDec(txtMedication.Text) < 0 Then
+            MessageBox.Show("Input Error the number cannot be negative.")
+            Return False
+
+
+        End If
+
+        If txtLabFees.Text = "" Then
+            MessageBox.Show("Input Error. Lab Fees cannot be blank")
+            Return False
+        ElseIf Not Decimal.TryParse(txtLabFees.Text, decTryParse) Then
+            MessageBox.Show("Input Error. Lab Fees must be entered as a decimal")
+            Return False
+        ElseIf CDec(txtLabFees.Text) < 0 Then
+            MessageBox.Show("Input Error the number cannot be negative.")
+            Return False
+
+        End If
+
+        If txtSurgicalCharges.Text = "" Then
+            MessageBox.Show("Input Error. Surgical Fees cannot be blank")
+            Return False
+        ElseIf Not Decimal.TryParse(txtSurgicalCharges.Text, decTryParse) Then
+            MessageBox.Show("Input Error. Surgical Fees must be entered as a decimal")
+            Return False
+        ElseIf CDec(txtSurgicalCharges.Text) < 0 Then
+            MessageBox.Show("Input Error the number cannot be negative.")
+            Return False
+
+        End If
+
+        If txtRehab.Text = "" Then
+            MessageBox.Show("Input Error. Rehab Fees cannot be blank")
+            Return False
+        ElseIf Not Decimal.TryParse(txtRehab.Text, decTryParse) Then
+            MessageBox.Show("Input Error. Rehan Fees must be entered as a decimal")
+            Return False
+        ElseIf CDec(txtRehab.Text) < 0 Then
+            MessageBox.Show("Input Error the number cannot be negative.")
+            Return False
+
+        End If
+
+        Return True
+    End Function
+End Class
